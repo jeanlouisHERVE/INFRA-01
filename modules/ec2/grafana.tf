bc111c6fcfe515469e9ba465767bbbb05b919b32
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-07-26 14:14:29 +0200

    INFRA- edit grafana ec2 tf file

--- modules/ec2/grafana.tf
+++ modules/ec2/grafana.tf
@@ -1,15 +1,20 @@
-resource "aws_instance" "grafana" {
-  ami           = "ami-07d9b9ddc6cd8dd30"
-  instance_type = "t2.micro"
-  iam_instance_profile = var.grafana_instance_profile
-  key_name      = "awstraining"
+resource "aws_key_pair" "training" {
+  key_name   = "training"
+  public_key = var.public_ssh_key
+}
 
-  tags = {
-    Name = "EC2_grafana"
-    Environment = "Sandbox"
-  }
+resource "aws_instance" "grafana" {
+  ami                    = "ami-07d9b9ddc6cd8dd30"
+  instance_type          = "t2.micro"
+  iam_instance_profile   = var.grafana_instance_profile
+  key_name               = aws_key_pair.training.key_name
 
   vpc_security_group_ids = [var.security_id_grafana, var.security_id_server]
+
+  tags = {
+    Name        = "EC2_grafana"
+    Environment = "Sandbox"
+  }
 
   provisioner "remote-exec" {
     inline = [
@@ -21,8 +26,8 @@ resource "aws_instance" "grafana" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("C:\\Users\\jeanl\\.ssh\\id_rsa")
-      host        = aws_instance.grafana.public_ip
+      private_key = file("C:/Users/jeanl/.ssh/id_rsa")
+      host        = self.public_ip
     }
-  } 
+  }
 }
