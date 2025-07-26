ee56b975c897c3ccdc47e1992e568da15de154b9
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-07-26 14:09:57 +0200

    INFRA- add ec2 instances

--- modules/ec2/grafana.tf
+++ modules/ec2/grafana.tf
@@ -1,28 +1,28 @@
-# resource "aws_instance" "grafana" {
-#   ami           = "ami-07d9b9ddc6cd8dd30"
-#   instance_type = "t2.micro"
-#   iam_instance_profile = var.grafana_instance_profile
-#   key_name      = "awstraining"
+resource "aws_instance" "grafana" {
+  ami           = "ami-07d9b9ddc6cd8dd30"
+  instance_type = "t2.micro"
+  iam_instance_profile = var.grafana_instance_profile
+  key_name      = "awstraining"
 
-#   tags = {
-#     Name = "EC2_grafana"
-#     Environment = "Sandbox"
-#   }
+  tags = {
+    Name = "EC2_grafana"
+    Environment = "Sandbox"
+  }
 
-#   vpc_security_group_ids = [var.security_id_grafana, var.security_id_server]
+  vpc_security_group_ids = [var.security_id_grafana, var.security_id_server]
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo apt update",
-#       "sudo apt install -y vim",
-#       "sudo apt install -y prometheus-node-exporter",
-#     ]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y vim",
+      "sudo apt install -y prometheus-node-exporter",
+    ]
 
-#     connection {
-#       type        = "ssh"
-#       user        = "ubuntu"
-#       private_key = file("C:\\Users\\jeanl\\.ssh\\id_rsa")
-#       host        = aws_instance.grafana.public_ip
-#     }
-#   } 
-# }
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("C:\\Users\\jeanl\\.ssh\\id_rsa")
+      host        = aws_instance.grafana.public_ip
+    }
+  } 
+}
