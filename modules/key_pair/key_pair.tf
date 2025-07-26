9a2818eb627fad6649d2b60db0ec8b6b32d04d62
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-07-26 13:17:12 +0200

    INFRA- add public ssh key variable

--- modules/key_pair/key_pair.tf
+++ modules/key_pair/key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "training" {
-  key_name   = "awstraining"
-  public_key = file("C:\\Users\\jeanl\\.ssh\\id_rsa.pub")
+  key_name   = "training"
+  public_key = var.PUBLIC_SSH_KEY
 }
