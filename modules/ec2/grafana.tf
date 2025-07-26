127e075962f17cdf338ebb8076a0a67268a3f9a9
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-07-26 16:10:32 +0200

    INFRA- edit grafana ec2 tf file

--- modules/ec2/grafana.tf
+++ modules/ec2/grafana.tf
@@ -1,13 +1,9 @@
-module "key_pair" {
-  source         = "./modules/key_pair"
-  public_ssh_key = var.public_ssh_key
-}
 
 resource "aws_instance" "grafana" {
   ami                    = "ami-07d9b9ddc6cd8dd30"
   instance_type          = "t2.micro"
   iam_instance_profile   = var.grafana_instance_profile
-  key_name               = aws_key_pair.training.key_name
+
 
   vpc_security_group_ids = [var.security_id_grafana, var.security_id_server]
 
