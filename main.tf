3cd93008a6bd256ea5e4587b42ea782592f0a2e8
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-07-27 08:21:06 +0200

    INFRA- add notes

--- main.tf
+++ main.tf
@@ -42,3 +42,13 @@ module "ec2_instances" {
   grafana_instance_profile    = module.roles.grafana_instance_profile
   prometheus_instance_profile = module.roles.prometheus_instance_profile
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "mon-bucket-terraform-state"
+    key            = "training/terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
