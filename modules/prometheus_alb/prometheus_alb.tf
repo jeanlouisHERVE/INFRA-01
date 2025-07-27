a991733d1494a958b14c90509760d29fb711b84f
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-07-27 21:38:46 +0200

    infra - comment alb

--- modules/prometheus_alb/prometheus_alb.tf
+++ modules/prometheus_alb/prometheus_alb.tf
@@ -1,43 +1,54 @@
-resource "aws_lb" "prometheus_alb" {
-  name               = var.name
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = var.security_groups
-  subnets            = var.subnets
-  enable_deletion_protection = false
-}
+# data "aws_availability_zones" "available" {}
 
-resource "aws_lb_target_group" "prometheus_alb" {
-  name        = "${var.name}-tg"
-  port        = var.target_port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"
+# resource "aws_subnet" "public" {
+#   count = 2
+#   vpc_id            = var.vpc_id
+#   cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+#   availability_zone = data.aws_availability_zones.available.names[count.index]
+#   ...
+# }
 
-  health_check {
-    path                = var.health_check_path
-    protocol            = "HTTP"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
 
-resource "aws_lb_listener" "prometheus_alb" {
-  load_balancer_arn = aws_lb.prometheus_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb" "prometheus_alb" {
+#   name               = var.name
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = var.security_groups
+#   subnets            = var.subnets
+#   enable_deletion_protection = false
+# }
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.prometheus_alb.arn
-  }
-}
+# resource "aws_lb_target_group" "prometheus_alb" {
+#   name        = "${var.name}-tg"
+#   port        = var.target_port
+#   protocol    = "HTTP"
+#   vpc_id      = var.vpc_id
+#   target_type = "instance"
 
-resource "aws_lb_target_group_attachment" "attachments" {
-  count             = length(var.target_instance_ids)
-  target_group_arn  = aws_lb_target_group.prometheus_alb.arn
-  target_id         = var.target_instance_ids[count.index]
-  port              = var.target_port
-}
+#   health_check {
+#     path                = var.health_check_path
+#     protocol            = "HTTP"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#   }
+# }
+
+# resource "aws_lb_listener" "prometheus_alb" {
+#   load_balancer_arn = aws_lb.prometheus_alb.arn
+#   port              = 80
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.prometheus_alb.arn
+#   }
+# }
+
+# resource "aws_lb_target_group_attachment" "attachments" {
+#   count             = length(var.target_instance_ids)
+#   target_group_arn  = aws_lb_target_group.prometheus_alb.arn
+#   target_id         = var.target_instance_ids[count.index]
+#   port              = var.target_port
+# }
--- modules/prometheus_alb/variables.tf
+++ modules/prometheus_alb/variables.tf
@@ -1,37 +1,42 @@
-variable "name" {
-  description = "Prometheus - ALB"
-  type        = string
-}
+# variable "name" {
+#   description = "Prometheus - ALB"
+#   type        = string
+# }
 
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
+# variable "vpc_id" {
+#   description = "VPC ID"
+#   type        = string
+# }
 
-variable "subnets" {
-  description = "List of subnet IDs"
-  type        = list(string)
-}
+# variable "subnets" {
+#   description = "List of subnet IDs"
+#   type        = list(string)
+# }
 
-variable "security_groups" {
-  description = "Security groups for the ALB"
-  type        = list(string)
-}
+# variable "security_groups" {
+#   description = "Security groups for the ALB"
+#   type        = list(string)
+# }
 
-variable "target_port" {
-  description = "Port to forward traffic to (e.g., Prometheus 9090)"
-  type        = number
-  default     = 9090
-}
+# variable "target_port" {
+#   description = "Port to forward traffic to (e.g., Prometheus 9090)"
+#   type        = number
+#   default     = 9090
+# }
 
-variable "health_check_path" {
-  description = "Path to check target health"
-  type        = string
-  default     = "/metrics"
-}
+# variable "health_check_path" {
+#   description = "Path to check target health"
+#   type        = string
+#   default     = "/metrics"
+# }
 
-variable "target_instance_ids" {
-  description = "List of EC2 instance IDs to register"
-  type        = list(string)
-  default     = []
-}
+# variable "target_instance_ids" {
+#   description = "List of EC2 instance IDs to register"
+#   type        = list(string)
+#   default     = []
+# }
+
+# variable "vpc_cidr_block" {
+#   description = "CIDR block of the VPC (e.g., 10.0.0.0/16)"
+#   type        = string
+# }
