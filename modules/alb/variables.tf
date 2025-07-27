018399a3762b74cd109a828dc7fe55fb31694725
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-07-27 20:57:36 +0200

    INFRA- create prometheus alb

---
+++ modules/alb/variables.tf
@@ -0,0 +1,37 @@
+variable "name" {
+  description = "Name of the ALB"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnets" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "security_groups" {
+  description = "Security groups for the ALB"
+  type        = list(string)
+}
+
+variable "target_port" {
+  description = "Port to forward traffic to (e.g., Prometheus 9090)"
+  type        = number
+  default     = 9090
+}
+
+variable "health_check_path" {
+  description = "Path to check target health"
+  type        = string
+  default     = "/metrics"
+}
+
+variable "target_instance_ids" {
+  description = "List of EC2 instance IDs to register"
+  type        = list(string)
+  default     = []
+}
