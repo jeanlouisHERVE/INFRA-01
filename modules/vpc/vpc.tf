b4fc6f6c653fa472e95889dbde396771632738de
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-08-02 08:47:09 +0200

    infra - add custom vpc

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,17 +1,22 @@
 
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "custom" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "custom-vpc"
+  }
 }
 
 resource "aws_subnet" "public" {
   count             = length(var.availability_zones)
-  vpc_id            = data.aws_vpc.default.id
-  cidr_block        = cidrsubnet(data.aws_vpc.default.cidr_block, 8, count.index)
+  vpc_id            = aws_vpc.custom.id
+  cidr_block        = cidrsubnet(aws_vpc.custom.cidr_block, 8, count.index)
   availability_zone = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-${count.index}"
+    Name = "custom-public-subnet-${count.index}"
   }
 }
 
