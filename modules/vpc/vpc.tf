a598be0e34a711b55c3d759222f37b7c34c56e5b
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-08-02 08:43:58 +0200

    infra - add 2 availibility zones

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -6,7 +6,7 @@ data "aws_vpc" "default" {
 resource "aws_subnet" "public" {
   count             = length(var.availability_zones)
   vpc_id            = data.aws_vpc.default.id
-  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index)
+  cidr_block        = cidrsubnet(data.aws_vpc.default.cidr_block, 8, count.index)
   availability_zone = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
