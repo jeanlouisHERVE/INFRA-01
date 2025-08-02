90d740c7a77f2768fb54ef6d5759c2cca088bfc3
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2025-08-02 08:51:50 +0200

    infra - add internet gateway

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -9,10 +9,10 @@ resource "aws_vpc" "custom" {
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.availability_zones)
-  vpc_id            = aws_vpc.custom.id
-  cidr_block        = cidrsubnet(aws_vpc.custom.cidr_block, 8, count.index)
-  availability_zone = var.availability_zones[count.index]
+  count                   = length(var.availability_zones)
+  vpc_id                  = aws_vpc.custom.id
+  cidr_block              = cidrsubnet(aws_vpc.custom.cidr_block, 8, count.index)
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
   tags = {
@@ -22,4 +22,30 @@ resource "aws_subnet" "public" {
 
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.custom.id
+  tags = {
+    Name = "custom-vpc-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.custom.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
