391fa20e9f3241cc73058bc4a8296617704b388c
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2025-08-11 10:19:56 +0200

    infra- add grafana alb

--- main.tf
+++ main.tf
@@ -100,15 +100,18 @@ module "prometheus_alb" {
   target_instance_ids = [data.aws_instances.prometheus.ids[0]]
   health_check_path   = "/-/healthy"
   target_port         = 9090
-  # cm_certificate_arn  = module.acm.certificate_arn
 }
 
-# module "grafana_alb" {
-#   source              = "./modules/alb"
-#   name                = "prometheus-alb"
-#   vpc_id              = "vpc-06ef89843ae777ffa"
-#   subnets             = ["subnet-0ce71756846452ea9"]
-#   security_groups     = ["sg-08bc5a66f905cd066", "sg-032a0ad9ae314e4eb"]
-#   target_instance_ids = ["i-0ac6f6ca558de626d"]
-#   target_port         = 3000
-# }
+module "grafana_alb" {
+  source  = "./modules/alb"
+  name    = "grafana-alb"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnet_ids
+  security_groups = [
+    module.security_groups.security_group_ids["grafana"],
+    module.security_groups.security_group_ids["server"]
+  ]
+  target_instance_ids = [data.aws_instances.grafana.ids[0]]
+  health_check_path   = "/-/healthy"
+  target_port         = 3000
+}
