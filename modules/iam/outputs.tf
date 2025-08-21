793a5d80c821ff1cfed29fac7340e0534825f216
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2025-08-21 10:14:21 +0200

    aws add sensitive to secrets

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,7 +1,9 @@
 output "aws_github_user_access_key_id" {
   value     = aws_iam_access_key.github_deployer_key.id
+  sensitive = true
 }
 
 output "aws_github_user_secret_access_key" {
   value     = aws_iam_access_key.github_deployer_key.secret
+  sensitive = true
 }
